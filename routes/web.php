5a693f70e54e15d1865220cc6bf5afa954b73cce
Gregor Medvesek <medvesek.gregor@gmail.com>
2025-07-17 23:34:53 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ["status" => "ok3"];
 });
+
+Route::get('/bonbon', function() {
+    return Bonbon::first();
+});
+
+Route::post('/bonbon', function() {
+    return Bonbon::create(["value" => Str::random(5)]);
+});
+
+
